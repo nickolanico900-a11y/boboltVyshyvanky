339117f9a69d2ab510845df28495e0c5f1a81820
nickolanico900-a11y <nickolanico900@gmail.com>
2025-11-06 21:50:18 +0200

    Updated App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -94,9 +94,9 @@ function App() {
       price: 1300,
       image: 'https://i.ibb.co/wFZ8XWSt/getman1.png',
       images: [
+'https://i.ibb.co/WWvMBbNT/getman42.png',
 'https://i.ibb.co/mCzN2y6K/getman44.png',
 'https://i.ibb.co/zT62DtGQ/getman41.png',
-'https://i.ibb.co/WWvMBbNT/getman42.png',
 'https://i.ibb.co/zhwzFC6S/getman43.png'
       ]
     },
